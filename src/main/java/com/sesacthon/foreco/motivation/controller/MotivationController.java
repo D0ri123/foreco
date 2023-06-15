c9eb6215696980a06beb63fb3a29b89a96f20abe
D0ri123 <lsh81550@gmail.com>
2023-06-16 01:23:45 +0900

    refactor: category id로 동기부여 데이터를 찾는다

--- src/main/java/com/sesacthon/foreco/motivation/controller/MotivationController.java
+++ src/main/java/com/sesacthon/foreco/motivation/controller/MotivationController.java
@@ -4,6 +4,7 @@ import com.sesacthon.foreco.motivation.dto.response.MotivationDetailDto;
 import com.sesacthon.foreco.motivation.service.MotivationService;
 import com.sesacthon.global.response.DataResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name="재활용 동기부여", description = "동기부여 관련 api")
 @RestController
 @RequiredArgsConstructor
 public class MotivationController {
@@ -18,8 +20,8 @@ public class MotivationController {
 
   @Operation(summary = "동기부여 정보", description = "재활용 가능한 품목에 대해서 동기부여(재활용 과정, 기대 결과물..) 제공을 합니다.")
   @GetMapping("/api/v1/motive")
-  public ResponseEntity<DataResponse<MotivationDetailDto>> getMotive(@RequestParam("trashId")Long trashId) {
-    MotivationDetailDto expectedResult = motivationService.findExpectedResult(trashId);
+  public ResponseEntity<DataResponse<MotivationDetailDto>> getMotive(@RequestParam("categoryId")Long categoryId) {
+    MotivationDetailDto expectedResult = motivationService.findExpectedResult(categoryId);
     return new ResponseEntity<>(DataResponse.of(HttpStatus.OK, "동기부여 정보 조회 성공", expectedResult), HttpStatus.OK);
   }
 
