33ead75acd219a19ab6315148f806537178cc5ff
D0ri123 <lsh81550@gmail.com>
2023-09-04 16:17:47 +0900

    chore: 코드 리뷰 반영

--- src/main/java/com/sesacthon/foreco/category/entity/RegionCategory.java
+++ src/main/java/com/sesacthon/foreco/category/entity/RegionCategory.java
@@ -2,6 +2,7 @@ package com.sesacthon.foreco.category.entity;
 
 import static jakarta.persistence.FetchType.*;
 
+import com.sesacthon.foreco.disposal.entity.Disposal;
 import com.sesacthon.foreco.region.entity.Region;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,6 +10,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -28,5 +32,8 @@ public class RegionCategory {
   @ManyToOne(fetch = LAZY)
   @JoinColumn(name = "region_id")
   private Region region;
+
+  @OneToMany(mappedBy = "regionCategory")
+  private List<Disposal> disposals = new ArrayList<>();
 
 }
