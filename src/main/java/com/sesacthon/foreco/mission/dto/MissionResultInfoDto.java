3354d4b60c4f75320175e4c755f0ef27886bbea2
hoiae <donald819@naver.com>
2023-10-31 11:24:14 +0900

    refactor: dto에 final키워드 추가

--- src/main/java/com/sesacthon/foreco/mission/dto/MissionResultInfoDto.java
+++ src/main/java/com/sesacthon/foreco/mission/dto/MissionResultInfoDto.java
@@ -1,20 +1,20 @@
 package com.sesacthon.foreco.mission.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class MissionResultInfoDto {
 
   /**
    * 지급할 포인트
    */
-  private Long rewardPoint;
+  private final Long rewardPoint;
   /**
    * 미션 수행 포인트가 반영된 이후의 유저가 보유한 포인트
    */
-  private Long userPoint;
+  private final Long userPoint;
 }
